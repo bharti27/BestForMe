1f56f5781da4b37bf2213b4bb5f050a68de2d2f5
Mamoke Ukulu <mamoke.k.ukulu@outlook.com>
2019-04-21 20:18:00 -0400

    Changed APP to class and added authenticatedUserName to the global state

--- src/APP.js
+++ src/APP.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component }from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import DashBoard from "./components/DashBoard";
 import {Login} from "./components/Login";
@@ -7,35 +7,45 @@ import {Favorites} from "./components/Favorites";
 import {Account} from "./components/Account";
 import NavBar from "./components/NavBar";
 
-function APP() {
-    return (
-        <Router>
-            <div>
-                <NavBar/>
-                <ul>
-                    <li>
-                        <Link to="/">index</Link>
-                    </li>
-                    <li>
-                        <Link to="/Dashboard">Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link to="/registration">Registration</Link>
-                    </li>
-                    <li>
-                        <Link to="/Favorites">Favorites</Link>
-                    </li>
-                </ul>
-                
-                <hr />
-                <Route exact path="/" component={ Login } />
-                <Route path="/dashboard" component={DashBoard} />
-                <Route path="/registration" component={Registration} />
-                <Route path="/favorites" component={Favorites} />
-                <Route path="/account" component={Account} />
-            </div>
-        </Router>
-    );
+class APP extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authenticatedUserName : ""
+        }
+    }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar/>
+                    <ul>
+                        <li>
+                            <Link to="/">index</Link>
+                        </li>
+                        <li>
+                            <Link to="/Dashboard">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/registration">Registration</Link>
+                        </li>
+                        <li>
+                            <Link to="/Favorites">Favorites</Link>
+                        </li>
+                    </ul>
+                    
+                    <hr />
+                    <Route exact path="/" component={ Login } />
+                    <Route path="/dashboard" component={DashBoard} />
+                    <Route path="/registration" component={Registration} />
+                    <Route path="/favorites" component={Favorites} />
+                    <Route path="/account" component={Account} />
+                </div>
+            </Router>
+        );
+    }
+    
 }
 
 export default APP;
