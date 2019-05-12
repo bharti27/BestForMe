e0eda9a9341c419e5deceac06ab7595d283fcf18
bharti27 <bharti2792@gmail.com>
2019-05-11 21:48:07 -0400

    changed route

---
+++ build/precache-manifest.26f1dfc5b085b85370e5626c72cd9bac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1607733c0be8ac557b8817c9fba594df",
+    "url": "/static/media/logo.1607733c.png"
+  },
+  {
+    "revision": "44cb8d5981fa351baf94ddd15eb4af93",
+    "url": "/static/media/Criterion-Collection.44cb8d59.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12ffcb8ba63f6fe034f0",
+    "url": "/static/js/main.64173027.chunk.js"
+  },
+  {
+    "revision": "6f4e6e0ff664c4ffa9c2",
+    "url": "/static/js/2.f621d25b.chunk.js"
+  },
+  {
+    "revision": "12ffcb8ba63f6fe034f0",
+    "url": "/static/css/main.8b64269b.chunk.css"
+  },
+  {
+    "revision": "6f4e6e0ff664c4ffa9c2",
+    "url": "/static/css/2.85ab5f83.chunk.css"
+  },
+  {
+    "revision": "02491a8452eda473856c128808d06118",
+    "url": "/index.html"
+  }
+];
